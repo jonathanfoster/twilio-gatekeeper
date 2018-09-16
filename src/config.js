c13b8303b2a9698f440f642c5e6425da8c1bc935
Jonathan Foster <jonathan.e.foster@gmail.com>
2018-09-16 16:35:26 -0400

    Default operator to my mobile number

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ module.exports = {
   accessTone: process.env.GATEKEEPER_ACCESS_TONE || '9',
   baseURL: process.env.GATEKEEPER_URL || 'https://gatekeeper.jonathanfoster.io',
   maxAttempts: process.env.GATEKEEPER_MAX_ATTEMPTS || 3,
-  operator: process.env.GATEKEEPER_OPERATOR || '786-355-8794',
+  operator: process.env.GATEKEEPER_OPERATOR || '202-297-6584',
   passcode: process.env.GATEKEEPER_PASSCODE || '1234',
 };
